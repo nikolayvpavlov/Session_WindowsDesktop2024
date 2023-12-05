41716bcf4b7799127b26ab1aa08cfbe068804f4a
Nikolay Pavlov <nikolay.pavlov@hotmail.com>
2023-12-05 12:14:21 +0200

    Finalizing the demo.

--- BWinFormsUsingWASDK/MainForm.Designer.cs
+++ BWinFormsUsingWASDK/MainForm.Designer.cs
@@ -28,18 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
+            progressBarBattery = new ProgressBar();
+            label1 = new Label();
+            label2 = new Label();
+            textBoxPowerSource = new TextBox();
             SuspendLayout();
+            // 
+            // progressBarBattery
+            // 
+            progressBarBattery.Location = new Point(113, 45);
+            progressBarBattery.Name = "progressBarBattery";
+            progressBarBattery.Size = new Size(675, 23);
+            progressBarBattery.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 45);
+            label1.Name = "label1";
+            label1.Size = new Size(79, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Battery Status";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 87);
+            label2.Name = "label2";
+            label2.Size = new Size(78, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Power source";
+            // 
+            // textBoxPowerSource
+            // 
+            textBoxPowerSource.Location = new Point(113, 84);
+            textBoxPowerSource.Name = "textBoxPowerSource";
+            textBoxPowerSource.Size = new Size(231, 23);
+            textBoxPowerSource.TabIndex = 3;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(textBoxPowerSource);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(progressBarBattery);
             Name = "MainForm";
             Text = "WinForms with Windows App SDK";
+            Load += MainForm_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ProgressBar progressBarBattery;
+        private Label label1;
+        private Label label2;
+        private TextBox textBoxPowerSource;
     }
 }
--- BWinFormsUsingWASDK/MainForm.cs
+++ BWinFormsUsingWASDK/MainForm.cs
@@ -5,7 +5,28 @@ namespace BWinFormsUsingWASDK
         public MainForm()
         {
             InitializeComponent();
-            //Microsoft.Windows.System.Power.PowerManager
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Microsoft.Windows.System.Power.PowerManager.RemainingChargePercentChanged += PowerManager_RemainingChargePercentChanged;
+            Microsoft.Windows.System.Power.PowerManager.PowerSourceKindChanged += PowerManager_PowerSourceKindChanged;
+        }
+
+        private void PowerManager_PowerSourceKindChanged(object? sender, object e)
+        {
+            Invoke(() =>
+            {
+                textBoxPowerSource.Text = Microsoft.Windows.System.Power.PowerManager.PowerSourceKind.ToString();
+            });
+        }
+
+        private void PowerManager_RemainingChargePercentChanged(object? sender, object e)
+        {
+            Invoke(() =>
+            {
+                progressBarBattery.Value = Microsoft.Windows.System.Power.PowerManager.RemainingChargePercent;
+            });
         }
     }
 }
